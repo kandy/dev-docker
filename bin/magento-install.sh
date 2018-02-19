bb7462a20e7447ef759a7c0fbcefcbd31c6c64e2
Andrii Kasian <akasian@magento.com>
2018-02-20 00:47:59 +0200

    Add more share servers && xdebug support

--- bin/magento-install.sh
+++ bin/magento-install.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
-docker-compose exec app bin/magento setup:install -vvv --db-name=magento2 --db-user=root  --db-password=root --db-host=db --base-url=http://mage.perf/ --use-rewrites=1 --admin-firstname=Store --admin-lastname=Owner --admin-email=admin@example.com --admin-user=admin --admin-password=123123q --cleanup-database --admin-use-security-key=0 --backend-frontname=admin
+docker-compose exec app php bin/magento setup:uninstall --no-interactive
+docker-compose exec db mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS magento2;"
+docker-compose exec app php bin/magento setup:install -vvv \
+    --db-name=magento2 \
+    --db-user=root  \
+    --db-password=root \
+    --db-host=db \
+    --base-url=http://mage.perf/ \
+	--use-rewrites=1 \
+	--admin-firstname=Store \
+	--admin-lastname=Owner \
+	--admin-email=admin@example.com \
+	--admin-user=admin \
+	--admin-password=123123q \
+	--cleanup-database \
+	--admin-use-security-key=0 \
+	--backend-frontname=admin \
+	--cache-backend=redis \
+	--cache-backend-redis-server=redis
